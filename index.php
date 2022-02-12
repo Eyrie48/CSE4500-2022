fa8555ce8bfe80dc1d27fd6b65064f51a2b9d7a4
Eyrie48 <eddiie6@hotmail.com>
2022-02-11 17:48:20 -0800

    update

--- index.php
+++ index.php
@@ -46,7 +46,20 @@
       <div id="education" class="content-item">
         <div class="vertical-center">
           <h4>Education
+              <?php foreach($json_data['education'] AS $education) { ?>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                  <div class="flex-grow-1">
+                    <h3 class="mb-0"><?php echo $education['school']; ?></h3>
+                    <div class="subheading mb-3"><?php echo $experience['degree']; ?></div>
+                    <p><?php echo $education['expected graduation']; ?></p>
+                  </div>
+                  
+                  <div class="flex-shrink-0">
+                    <span class="text-primary"><?php echo $education['period']; ?></span>
+                  </div>
 
+                </div>
+              <?php  } ?>
           </h4>
           <p>...</p>
         </div>
